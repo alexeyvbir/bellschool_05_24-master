afc3ad65d6f2eeee666dc93479ded700c559c43c
Alexey Biryukov <alexeyvbir@gmail.com>
2024-05-13 13:33:43 +0300

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    public void demo(){
+        System.out.print("Кириллица!");
+
+        for(int i = 1; i <= 5; ++i) {
+            System.out.println("i = " + i);
+        }
+
+    }
+
+}
